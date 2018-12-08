ce5012dea4e34a9ed0461e1dcf230270d5f06ff5
Edmund Noble <edmundnoble@gmail.com>
2018-12-08 00:50:49 -0500

    Remove cycles

--- src/J/CLI.hs
+++ src/J/CLI.hs
@@ -6,9 +6,6 @@ import Control.Applicative((<|>))
 import Data.Semigroup((<>))
 import qualified Options.Applicative as Options
 
-import J.Cycles.Types
-
-import qualified J.Cycles.Main as Cycles
 import qualified J.Indexing.Main as Indexing
 
 refresh :: Options.Parser (IO ())
@@ -17,21 +14,9 @@ refresh = Options.flag' Indexing.refreshIndex
   <> Options.help "Refresh doc tags"
   )
 
-view :: Options.Parser (ViewerConfig -> IO ())
-view = Options.flag' Cycles.brickMain
-  (  Options.long "view"
-  <> Options.help "View cycle history"
-  )
-
 logPath :: Options.Parser FilePath
 logPath =
-  Options.strOption ((Options.long "cyclelog") <> Options.value "cyclelog")
-
--- day :: Options.Parser (Maybe Day)
--- day = Options.optional $ Options.option Options.auto
---   (  Options.long "day"
---   <> Options.help "Day to view (on the right)"
---   )
+  Options.strOption $ Options.long "cyclelog" <> Options.value "cyclelog"
 
 width :: Options.Parser Int
 width = Options.option Options.auto (Options.long "width" <> Options.value 7)
@@ -41,17 +26,8 @@ width = Options.option Options.auto (Options.long "width" <> Options.value 7)
 --     [(r, "")] -> return r
 --     _         -> Left $ "cannot parse value `" ++ arg ++ "'"
 
-viewerConfig :: Options.Parser ViewerConfig
-viewerConfig = do
-  lp <- logPath
-  w <- width
-  return ViewerConfig {
-    _configLogPath = lp
-  , _configIntervalSize = w
-  }
-
 cli :: Options.Parser (IO ())
-cli = (view <*> viewerConfig) <|> refresh
+cli = refresh
 
 opts :: Options.ParserInfo (IO ())
 opts = Options.info (cli Options.<**> Options.helper)
