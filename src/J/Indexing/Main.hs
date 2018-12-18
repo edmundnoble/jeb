ccff036971d95498cdf218d3b95ce622d82c1d96
Edmund Noble <edmundnoble@gmail.com>
2018-12-18 16:55:04 -0500

    Doc update

--- src/J/Indexing/Main.hs
+++ src/J/Indexing/Main.hs
@@ -73,7 +73,7 @@ fsLinker tagsFolder docsFolder = Linker (traverse_ fsLink)
                 Dir.createDirectoryIfMissing False tagsFolder
                 writeTagFS tagsFolder docsFolder fs
 
--- | Compute a diff from `docsFolder` onto the filesystem in `tagsFolder`,
+-- | Compute a diff from the filesystem in `tagsFolder`,
 dryRunLinker :: FilePath -> Linker (IO ())
 dryRunLinker tagsFolder = Linker (dryRunLink)
         where
